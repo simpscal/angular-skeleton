dfc8e158e52c8d3ae7f23f8e8122da57f18f0223
Tu Phan <pt27122000@gmail.com>
2025-04-02 23:34:08 +0700

    chore: configure module federation and update build settings for shell app

---
+++ apps/shell/src/bootstrap.ts
@@ -0,0 +1,14 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import APP_PROVIDERS from './app/app.providers';
+import { environment } from '@environment';
+
+if (environment.isProduction) {
+    enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+    providers: APP_PROVIDERS
+}).catch((error) => console.error(error));
--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,14 +1,1 @@
-import { enableProdMode } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-import { AppComponent } from './app/app.component';
-import APP_PROVIDERS from './app/app.providers';
-import { environment } from '@environment';
-
-if (environment.isProduction) {
-    enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-    providers: APP_PROVIDERS
-}).catch((error) => console.error(error));
+import('./bootstrap').catch((err) => console.error(err));
